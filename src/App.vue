106745095f798807bfdeacb1e9761cd00b36d3d6
L I L V C <simidawalang@gmail.com>
2023-01-11 15:08:49 +0100

    added method to script tag

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-bind:style="{ 'font-size': `${size}px` }">
+      {{ name }} <span v-text="surname"></span>
+    </p>
+
+    <button @click="toggleName">Change name</button>
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            name: "Simi",
+            surname: "Da-Walang",
+            size: 50,
+        };
+    },
+    methods: {
+      toggleName() {
+        this.name = 'Siri'
+      }
+    },
+    components: HelloWorld,
+};
 </script>
 
 <style>
